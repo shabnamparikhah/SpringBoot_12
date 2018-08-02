0757bcbfa40503310760be7d1764b62896cf2536
GBTC441012UR <GBTC441012UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-08-02 15:17:27 -0400

    uploadImage

---
+++ src/main/java/com/example/demo/Actor.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Actor {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    private String name;
+    @NotNull
+    private String realname;
+    @NotNull
+    private String headshot;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public String getHeadshot() {
+        return headshot;
+    }
+
+    public void setHeadshot(String headshot) {
+        this.headshot = headshot;
+    }
+}
